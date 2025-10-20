e728bfaa7f5b7ca4695cde31e26f82326125601a
riccardobonagura <191904345+riccardobonagura@users.noreply.github.com>
2025-10-20 20:38:24 +0200

    Enhance Test.java with comments on threading
    Added comments to clarify the role of ExecutorService and Runnable in multithreading.

--- Esercizi/02_JAVA/01_Multithreading/ThreadPool_example/src/Test.java
+++ Esercizi/02_JAVA/01_Multithreading/ThreadPool_example/src/Test.java
@@ -4,11 +4,17 @@ import java.util.concurrent.Executors;
 public class Test {
  
     public static void main(String[] args) {
+     
+        //executor è come il thread che prende in carico la funzione in Python, 
+        //in questo caso la claase Runnable è la funzione ed ExecutorService è il thread.
         ExecutorService executor = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 10; i++) {
+        
+     for (int i = 0; i < 10; i++) {
             Runnable worker = new WorkerThread("" + i);
+            // target = worker
             executor.execute(worker);
           }
+        //ogni thread che ritorna si chiude
         executor.shutdown();
         while (!executor.isTerminated()) {
         }
