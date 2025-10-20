b8336a38a5ac7ba08f72bbf09c7016220cf0e9a7
riccardobonagura <191904345+riccardobonagura@users.noreply.github.com>
2025-10-20 20:28:51 +0200

    Improve comments and code formatting in WriterThread
    Refactor comments and formatting in WriterThread class

--- Esercizi/02_JAVA/01_Multithreading/ThreadPipe/src/threadpipe/WriterThread.java
+++ Esercizi/02_JAVA/01_Multithreading/ThreadPipe/src/threadpipe/WriterThread.java
@@ -7,31 +7,28 @@ import java.io.*;
  * fa output sulla pipe
  */
 
+//classe che eredita da Thread
 public class WriterThread extends Thread {
-	
+
+	//oggetto 'Stream' generico di comunicazione
 	private DataOutputStream dataOut;
 	
-	
+	//costruttore 
 	public WriterThread ( PipedOutputStream pipeOut ){
-		dataOut  = new DataOutputStream ( pipeOut );
-		
+		dataOut  = new DataOutputStream ( pipeOut );	
 	}
 	
 	
 	public void run (){
 		
-		// BufferedReader per lettura da System.in
+		// BufferedReader: oggetto incaricato della lettura da System.in
 		BufferedReader keyboardBuf = new BufferedReader ( new InputStreamReader ( System.in ) );
 		String s;
 		
 		while ( true ){
-			try{
-				System.out.println ( "[Writer] enter string: "  );
-				
+			try{				
 				// lettura stringa
 				s = keyboardBuf.readLine();
-				System.out.println ( "[Writer] string: < " + s  + " > output to pipe");
-
 				// output su pipe
 				dataOut.writeUTF(s);
 				
@@ -40,5 +37,4 @@ public class WriterThread extends Thread {
 			}
 		}	
 	}
-
 }
